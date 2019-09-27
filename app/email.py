63ac58627d6372b9b6477342f4f16598050e21bf
Eric Seidler <eric.seidler@wwt.com>
2019-09-27 10:55:39 -0500

    install flask-bootstrap for website redesign

--- app/email.py
+++ app/email.py
@@ -1,19 +1,13 @@
 from flask import render_template
 from flask_mail import Message
-from threading import Thread
 from app import app, mail
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_email, args=(app, msg)).start()
+    mail.send(msg)
 
 
 def send_password_reset_email(user):
