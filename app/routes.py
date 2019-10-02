b739494ab3afc6a3b1fdcf0dd62c16a2739e4c87
Eric Seidler <eric.seidler@wwt.com>
2019-10-02 16:00:48 -0500

    add language prop to posts and assign post a language when first created

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from flask_babel import _, get_locale
+from guess_language import guess_language
 from app import app, db
 from app.email import send_password_reset_email
 from app.forms import (
@@ -22,7 +23,10 @@ from app.models import User, Post
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == "UNKNOWN" or len(language) > 5:
+            language = ""
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_("Your post is now live!"))
