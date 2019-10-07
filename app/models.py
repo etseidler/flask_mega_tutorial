56c2f8d39089721921ffb4c688a47af0364d2426
Eric Seidler <eric.seidler@wwt.com>
2019-10-07 13:15:48 -0500

    add notifications model to db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import json
 import jwt
 from datetime import datetime
 from flask import current_app
@@ -86,6 +87,7 @@ class User(UserMixin, db.Model):
         lazy="dynamic",
     )
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship("Notification", backref="user", lazy="dynamic")
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -173,3 +175,13 @@ class Message(db.Model):
     def __repr__(self):
         return "<Message {}>".format(self.body)
 
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
